5e84e9d33ad7fd2daacdf600409ca1b62d544f36
Marek Szuba <mszuba01@gmail.com>
2020-08-28 16:04:47 +0200

    add support for keyboards

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('swipeUp', () => scroller.scroll(1));
   document.addEventListener('swipeDown', () => scroller.scroll(-1));
 
+  document.addEventListener('keydown', (e) => {
+
+    switch (e.keyCode) {
+      case 40:
+        return scroller.scroll(1)
+        break;
+      case 38:
+        return scroller.scroll(-1)
+        break;
+      default:
+        break;
+    }
+  })
+
 })
